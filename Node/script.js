90d175413b860687ec9a0d726b1f41f8ae163da0
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-11-06 14:42:14 +0530

    npm unistall nodemon...npm install nodemon -d -do this

--- Node/script.js
+++ Node/script.js
@@ -30,7 +30,10 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 7040;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);//can give any number as port
 });
+//unistalled nodemon
+
+//instaled nodemon -d
